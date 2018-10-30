f89deae46a8ac9f13137831cfbeb12c430c782fe
nuulu143 <Niiaz_uulu@comcast.com>
2018-10-30 17:24:29 -0400

    Cheater part 2

--- remove-zeros/solution.js
+++ remove-zeros/solution.js
@@ -15,3 +15,24 @@ function removeZeros(array) {
 Hahahaha....
 
 removeZeros = arr => arr['fi' + 'lter'](e => e !== 0 && e !== '0')['conc' + 'at'](arr['filt' + 'er'](e => e === 0 || e === '0'));
+
+LoooooooL
+
+Array.prototype.addEnd = Array.prototype.push;
+Array.prototype.addBegin = Array.prototype.unshift;
+
+function removeZeros(array) {
+  let res = [];
+  for (let i = array.length; i--;) {
+    if (array[i] !== 0 && array[i] !== '0') {
+      res.addBegin(array[i])
+    }
+  }
+  
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 0 || array[i] === '0') {
+      res.addEnd(array[i])
+    }
+  }
+  return res;
+}
